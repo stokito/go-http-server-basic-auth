b36b6749fe00ba09134e9e35f6841d450998253b
Sergey Ponomarev <stokito@gmail.com>
2022-04-16 22:37:48 +0300

    cors_handler.go

--- cors_handler.go
+++ cors_handler.go
@@ -8,14 +8,16 @@ type CorsHandlerWrapper struct {
 
 func (h *CorsHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var (
-		corsAllowHeaders     = "authorization"
-		corsAllowMethods     = "HEAD,GET,POST,PUT,DELETE,OPTIONS"
 		corsAllowOrigin      = "*"
+		corsExposeHeaders    = "Date"
+		corsAllowMethods     = "HEAD,GET,POST,PUT,DELETE,OPTIONS"
+		corsAllowHeaders     = "Accept,Authorization,Date,Content-Type,Origin"
 		corsAllowCredentials = "true"
 	)
-	h.Handler.ServeHTTP(w, r)
-	w.Header().Set("Access-Control-Allow-Credentials", corsAllowCredentials)
-	w.Header().Set("Access-Control-Allow-Headers", corsAllowHeaders)
-	w.Header().Set("Access-Control-Allow-Methods", corsAllowMethods)
 	w.Header().Set("Access-Control-Allow-Origin", corsAllowOrigin)
+	w.Header().Set("Access-Control-Expose-Headers", corsExposeHeaders)
+	w.Header().Set("Access-Control-Allow-Methods", corsAllowMethods)
+	w.Header().Set("Access-Control-Allow-Headers", corsAllowHeaders)
+	w.Header().Set("Access-Control-Allow-Credentials", corsAllowCredentials)
+	h.Handler.ServeHTTP(w, r)
 }
