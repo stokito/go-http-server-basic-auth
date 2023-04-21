db8d19a2f8990c292c196033edba2761086c7484
Sergey Ponomarev <stokito@gmail.com>
2023-04-21 22:43:39 +0300

    cors_handler.go: add cors header only if Origin present

--- cors_handler.go
+++ cors_handler.go
@@ -8,14 +8,16 @@ type CorsHandlerWrapper struct {
 
 func (h *CorsHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	origin := r.Header.Get("Origin")
-	header := w.Header()
-	header.Set("Access-Control-Allow-Origin", origin)
-	header.Set("Access-Control-Allow-Credentials", "true")
-	if r.Method == http.MethodOptions {
-		header.Set("Access-Control-Allow-Methods", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
-		header.Set("Access-Control-Allow-Headers", "Accept,Authorization,Content-Type,Date,Origin,X-Requested-With")
-		w.WriteHeader(http.StatusNoContent)
-		return
+	if origin != "" {
+		header := w.Header()
+		header.Set("Access-Control-Allow-Origin", origin)
+		header.Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == http.MethodOptions {
+			header.Set("Access-Control-Allow-Methods", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
+			header.Set("Access-Control-Allow-Headers", "Accept,Authorization,Content-Type,Date,Origin,X-Requested-With")
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
 	}
 	h.Handler.ServeHTTP(w, r)
 }
